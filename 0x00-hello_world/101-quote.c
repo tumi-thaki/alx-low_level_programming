d1f0c2fa7035dfd26f28ec359ebbea304655b67b
tumi-thaki <tumiethaki01@gmail.com>
2023-08-10 10:23:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints using puts function
+ * Return: 1 for success alway
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
