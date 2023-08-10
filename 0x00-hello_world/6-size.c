50eaf234af5e3b90e2efc8e873595fc00589e68b
tumi-thaki <tumiethaki01@gmail.com>
2023-08-10 09:49:35 -0700

    tasks error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s) \n", sizeof(j));
 	printf("Size of a long long: %ld byte(s) \n", sizeof(k));
 	printf("Size of a float: %ld byte(s) \n", sizeof(f));
+	fprintf(stderr);
 	return (0);
 }
