0c77b7d30b0a67efe7e2f5d868180295a56a3283
tumi-thaki <tumiethaki01@gmail.com>
2023-08-10 07:29:54 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Date: 10/08/2023
+ * Author: Tumisang Thaki
+ * Description: a c program that prints using a 'puts' funtion
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
