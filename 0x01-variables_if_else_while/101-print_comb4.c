dc50757d111f7e1a810b2e33990efb5010c72378
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 11:53:27 -0700

    task 1 advanced tasks 2 reupload 0

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 
 	for (num1 = 0; num1 < 8; num1++)
 	{
-		for (num2 = 0; num2 < 9; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (num3 = 0; num3 < 10; num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
