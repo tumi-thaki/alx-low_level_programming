e46ad3f8063e0a17b2dad69eb7919b4e4f9b473b
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 10:42:49 -0700

    task 5 reupload 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,10 @@ int main(void)
 {
 	int num = 0;
 
-	for (num = 0; num < 9; num++)
-	       printf("%d", num);
-
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
 	putchar('\n');
 	return (0);
 }
