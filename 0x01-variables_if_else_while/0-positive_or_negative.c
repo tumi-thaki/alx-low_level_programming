27851834660700ba75ef0c30fff2d166a8d378cd
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 11:34:43 -0700

    task 0 betty coding 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,6 @@ int main(void)
 {
 	int n;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -18,7 +17,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	       	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
