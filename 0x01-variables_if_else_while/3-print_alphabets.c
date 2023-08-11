d88f8d6bdef212c3dee71ef1215c1abc9d4888ba
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 00:35:52 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point,prints lowersace then uppercase alphabets
+ * Return: 0 always for success
+*/
+int main(void)
+{
+	char c = 97;
+	while (c < 123)
+	{
+		putchar(c);
+		c++;
+	}
+	c = 65;
+	while (c < 92)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point, prints all lowercase alphabets except q and e
+ * Return: 0 always for success
+*/
+int main(void)
+{
+	char c = 97;
+	while (c < 123)
+	{
+		if (c == 101)
+		{
+			c++;
+		}
+		else if (c == 113)
+		{
+			c++
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
