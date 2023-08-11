18950770225000ed9562a6f424df678be1732ff6
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 12:00:16 -0700

    task 1 advanced tasks 3 reupload 0

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 		{
 			putchar(num1 / 10 + '0');
 			putchar(num1 % 10 + '0');
-			putchar('');
+			putchar(' ');
 			putchar(num2 / 10 + '0');
 			putchar(num2 % 10 + '0');
 			if (num1 != 99 || num2 != 99)
