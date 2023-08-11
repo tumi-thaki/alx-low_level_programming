b0f8c5591e1f71d30b1920204f87096632a2cf1d
tumi-thaki <tumiethaki01@gmail.com>
2023-08-11 11:46:30 -0700

    task 1 advanced tasks 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,12 @@ int main(void)
 			putchar(num1 + '0');
 			putchar(num2 + '0');
 			if (num1 != 8 || num2 != 9)
-			{
+999988888888			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
