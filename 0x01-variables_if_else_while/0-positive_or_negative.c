274b443862bfa68536207510a40d48b2d1a344a5
tumi-thaki <tumiethaki01@gmail.com>
2023-08-10 23:48:35 -0700

    task 1"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		puts("is positive");
+		puts("is positive \n");
 	if (n = 0)
-		puts("is zero");
+		puts("is zero \n");
 	if (n < 0)
-		puts("is negative");
+		puts("is negative \n");
 	return (0);
 }
