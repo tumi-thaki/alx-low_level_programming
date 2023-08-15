4044d81d5651a3aec12f1240e3aac4d669fe7015
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 14:23:43 -0700

    corrections advanced tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,10 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (i = 0; i >= n; i++)
+		for (i = 0; i > n; i++)
 		{
 			_putchar('0');
-			for (j = 0; j >= n; j++)
+			for (j = 1; j > n; j++)
 			{
 				product = i * j;
 				if (product < 10)
