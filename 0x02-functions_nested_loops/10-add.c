f12b989749c8b4566d4c3e199266b8e079660482
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 08:59:07 -0700

    correction update task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,12 @@
 /**
  * add - adds two numbers
  * @ num1 - argument to be added
- * @ num2 - argument to be added
+ * @num2: argument to be added
  * Return: sum of two numbers
  *
 */
 
-int add(int num1,  int num2)
+int add(int num1, int num2)
 {
-	return(num1 + num2);
+	return (num1 + num2);
 }
