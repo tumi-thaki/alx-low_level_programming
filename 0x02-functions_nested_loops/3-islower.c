df188c51f60cecc2f7af38965fd53751c81ee045
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 05:26:34 -0700

    task 3 correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks if a character is lowercase
- * @c - argument to be checked
+ * @c - argument to be compared for lowercase 
  * Return: 1 if c is lowercase 0 otherwise
 */
 
