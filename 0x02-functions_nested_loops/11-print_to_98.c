f898fd3d1450e00e335eb7cb21b8caf7b1708bd0
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 09:12:04 -0700

    correction update task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,9 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (i = n; i <= 98; i--)
+		{
 			printf("%d, ", n);
+		}
 	}
 	else if (n == 98)
 	{
@@ -24,7 +26,9 @@ void print_to_98(int n)
 	else
 	{
 		for (i = n; i >= 98; i++)
+		{
 			printf("%d, ", n);
+		}
 	}
 	printf("\n");
 }
