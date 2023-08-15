9049a3ebf5755ee64ee496e4c0f99076f6003925
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 05:09:36 -0700

    correcrion task 1 & 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@ int main(void)
 	char string[] = "_putchar";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(string[i]);
 	_putchar('\n');
 	return (0);
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@ void print_alphabet(void)
 {
 	char c = 97;
 
-	for (c = 97; c < 123: c++)
+	for (c = 97; c < 123; c++)
 		_putchar(c);
 	_putchar('\n');
 }
