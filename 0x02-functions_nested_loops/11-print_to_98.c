ff84aa12ccef0af521f5b1f5cab3274a02a56377
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 11:20:55 -0700

    corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if ( i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("98");
+			}
 		}
 	}
 	else if (n == 98)
@@ -27,7 +34,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("98");
 		}
 	}
 	printf("\n");
