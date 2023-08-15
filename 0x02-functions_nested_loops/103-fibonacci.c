401a9d8fee806aab552ebe56f22b9f4fc208f89e
tumi-thaki <tumiethaki01@gmail.com>
2023-08-15 13:47:10 -0700

    task 16

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int num1 = 0, num2 = 1, num3;
-	unsigned long sum = 1;
+	unsigned long sum;
 
-	while(num3 < 4000000)
+	while (num3 < 4000000)
 	{
 		num3 = num1 + num2;
 		num1 = num2;
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,5 +8,29 @@
 
 int main(void)
 {
-	unsigned long int i, j = 1, k = 2;
+	unsigned long int i, j = 1, k = 2, j1, j2, k1, k2;
+
+	printf("%lu", j);
+	        for (i = 1; i < 91; i++)
+         {
+                 printf(", %lu", k);
+                 k = k + j;
+                 j = k - j;
+
+         }
+         j1 = j / 1000000000;
+         j2 = j % 1000000000;
+         k1 = k / 1000000000;
+         k2 = k % 1000000000;
+         for (1 = 92; 1 < 99; ++i)
+         {
+                 printf(", %lu", k1 + (k2 / 1000000000));
+                 printf("%lu", k2 % 10000000);
+                 k1 = k1 + j1;
+                 j1 = k1 - j1;
+                 k2 = k2 + j2
+                         j2 = k2 - j2;
+         }
+         printf("\n");
+         return (0);
 }
