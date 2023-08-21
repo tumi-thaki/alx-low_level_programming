1ca19e46424b61d51d6a542a80b00b0c16de4427
tumi-thaki <tumiethaki01@gmail.com>
2023-08-21 12:43:36 -0700

    task 10 append

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,11 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 		else if ((s[i] >= '0') && (s[i]  <= '9'))
+		{
 			num = (num * 10) + (s[i] - '0');
+			if (s[i +1] == ' ')
+				break;
+		}
 		i++;
 	}
 	return (num * sign);
