1c771a01dbe141dfbcdc9b699838ea34b34f7135
tumi-thaki <tumiethaki01@gmail.com>
2023-09-05 07:20:31 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
+ * _strdup - copies argument
+ * @str: argument
+ * Return: pointer to allocated space memory
  *
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *a;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	a = (char *)(malloc((i + 1) * sizeof(char)));
+
+	if (a == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+	{
+		a[j] = str[j];
+		j++;
+	}
+
+	return (a);
+}
