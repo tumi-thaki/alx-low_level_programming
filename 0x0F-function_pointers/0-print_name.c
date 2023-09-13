e9b26634adf390739850c62c11adec37b15f5f63
tumi-thaki <tumiethaki01@gmail.com>
2023-09-13 11:35:06 -0700

    betty fxed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
-/** 
-  * print_name - function to print name 
-  * 
-  * @name: name to print 
-  * @f: function pointer that doesn't return anything 
-  * 
-  * Return: Always 0 (Success) 
- */ 
-  
- void print_name(char *name, void (*f)(char *)) 
- { 
-         if (name && f) 
-                 f(name); 
- }
+/**
+ * print_name - function to print name
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ * Return: 0 always for success
+ *
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
