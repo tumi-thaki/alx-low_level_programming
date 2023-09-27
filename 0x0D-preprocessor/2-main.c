03e5c12b3f3066969f261626f3eb1cf93b277eda
tumi-thaki <tumiethaki01@gmail.com>
2023-09-27 11:58:08 -0700

    Mandatpry tasks upload

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+nclude <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,4 @@
+GENERAL PROGRAMS
+what are macros and how to use them
+What are the most common predefined macros
+How to include guard your header files
